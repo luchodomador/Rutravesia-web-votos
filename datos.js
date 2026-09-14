03294b3d316643f8e32125a912e2712f83d9ded8
luchodomador <domador.ucv@gmail.com>
2026-09-14 17:40:52 -0400

    Add files via upload

--- datos.js
+++ datos.js
@@ -60,7 +60,7 @@ const catalogoGeneral = [
         precio: "10$ por persona ",
         imagen: "assets/ImagenAnaucos.png",
        fechasPropuestas: [
-        { fecha: "Domingo 27 de Septiembre", interesados: 7 },
+        { fecha: "Domingo 27 de Septiembre", interesados: 9 },
         { fecha: "Domingo 4 de Octubre", interesados: 0 }
         ],
         imagenes: [
